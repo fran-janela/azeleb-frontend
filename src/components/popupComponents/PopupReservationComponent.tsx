66e5b73b4e698a306caaf1e0c11c8dc39fc844df
fran-janela <francisco.pinheiro.janela@gmail.com>
2021-08-06 01:14:14 -0300

    Nome logado

--- src/components/popupComponents/PopupReservationComponent.tsx
+++ src/components/popupComponents/PopupReservationComponent.tsx
@@ -4,11 +4,11 @@ import { AuthContext } from "../../context/AuthContext";
 import style from "../../styles/components/PopupComponents.module.css";
 
 export default function LoginPopup(props: any) {
-  const { logOut } = useContext(AuthContext);
+  const { logOut, user } = useContext(AuthContext);
 
   return (
     <div className={style.reservationContainer}>
-      <div className={style.loginTag}>SEUS AGENDAMENTOS</div>
+      <div className={style.loginTag}>Bom dia, {user.name}!</div>
       <div className={style.reservationText}>
         O seu agendamento não está disponível no MVP de autenticação
       </div>
